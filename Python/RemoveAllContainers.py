1495482e5af9876b6efb16af00efbd0ff5f6531d
deepaksharma066 <85190366+deepaksharma066@users.noreply.github.com>
2021-08-26 00:08:02 +0530

    Create RemoveAllContainers.py

---
+++ Python/RemoveAllContainers.py
@@ -0,0 +1,12 @@
+import subprocess
+import cgi
+
+print("content-type: text/html")
+print("Access-Control-Allow-Origin: *")
+print()
+
+
+doc = "sudo docker rm -f `docker ps -a -q`"
+
+cmd = subprocess.getoutput(doc)
+print(cmd)
